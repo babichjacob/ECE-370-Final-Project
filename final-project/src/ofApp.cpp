ca9b3a07b17aa7e7a77458f45b8acbb8528e0e2c
Jacob Babich <jacobbabichpublic+git@gmail.com>
2018-12-03 17:25:41 -0500

    test `ofApp::mousePressed` method

--- final-project/src/ofApp.cpp
+++ final-project/src/ofApp.cpp
@@ -40,7 +40,7 @@ void ofApp::mouseDragged(int x, int y, int button){
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
-
+	ofSetBackgroundColor(rand() % 256, rand() % 256, rand() % 256);
 }
 
 //--------------------------------------------------------------
