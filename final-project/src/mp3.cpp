94b7dd972e1c815833f5c9191e963000b9849572
Jacob Babich <jacobbabichpublic+git@gmail.com>
2018-12-07 18:09:43 -0500

    get title from MP3 file

--- final-project/src/mp3.cpp
+++ final-project/src/mp3.cpp
@@ -1,4 +1,5 @@
 #include "mp3.h"
+#include "ID3v2.h"
 
 #include <filesystem>
 #include <fstream>
@@ -18,23 +19,30 @@ void mp3_main() {
 
 	string music_path = "bin/data/Music";
 	filesystem::recursive_directory_iterator music_directory = filesystem::recursive_directory_iterator(music_path);
+	ID3v2::AbstractFrame                          *frame;
 
 	// This is a foreach loop (much more modern than for loops)
 	for (auto &path : music_directory) {
 		cout << "mp3_main: Found a file: " << path.path() << endl;
+		ID3v2::Tag mp3_tag(path.path().string());
+		ID3v2::AbstractFrame *frame;
+		vector<ID3v2::AbstractFrame *> frames;
+		vector< ID3v2::AbstractFrame * >::iterator iter;
 
-		ifstream mp3_file(path.path(), ifstream::in | ifstream::binary);
+		if (mp3_tag.IsValid()) {
+			cout << "ID3v2 version: " << mp3_tag.GetVersion().GetStringValue() << endl;
+			frames = mp3_tag.GetFrames();
 
-		// Skip first 6 bytes (until we end up at byte 7)
-		mp3_file.ignore(6);
-
-		char header_length[4];
-		mp3_file.read(header_length, 4);
-
-		int header_length_as_int = chars_to_int(header_length);
-
-		printf("%d\n", header_length_as_int);
-
-		mp3_file.close();
+			ID3v2::Frame::v23::TIT2 *tit2;
+			tit2 = dynamic_cast<ID3v2::Frame::v23::TIT2 *>(mp3_tag.GetFrameWithName("TIT2"));
+			cout << (tit2 == nullptr ? "Null pointer" : "Not null pointer") << endl;
+			const uint8_t *data = tit2->GetData();
+			int data_length = tit2->GetSize();
+			string data_str = "";
+			for (int i = 3; i < data_length; i++) {
+				if ((data[i] != 0))	data_str += data[i];
+			}
+			cout << "Title: " << data_str << endl;
+		}
 	}
 }
