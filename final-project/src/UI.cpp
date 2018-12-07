16f2c6d4f87a98911582728753537250de363a04
Jacob Babich <jacobbabichpublic+git@gmail.com>
2018-12-06 21:19:27 -0500

    add fonts

--- final-project/src/UI.cpp
+++ final-project/src/UI.cpp
@@ -12,8 +12,15 @@ UI::~UI()
 }
 
 void UI::setup() {
+	font_small.load("Heebo/Heebo-Regular.ttf", font_small_size);
+	font_medium.load("Heebo/Heebo-Light.ttf", font_medium_size);
+	font_large.load("Heebo/Heebo-Thin.ttf", font_large_size);
+
+
 	menu_bar.x = 0;
 	menu_bar.y = 0;
+
+
 
 	num_menu_bar_top_level_entries = 4;
 
--- final-project/src/UI.h
+++ final-project/src/UI.h
@@ -12,9 +12,20 @@ public:
 	void draw();
 
 private:
+	ofTrueTypeFont font_small;
+	int font_small_size = 9;
+
+	ofTrueTypeFont font_medium;
+	int font_medium_size = 13;
+
+	ofTrueTypeFont font_large;
+	int font_large_size = 18;
+
 	ofRectangle menu_bar;
 
 	int num_menu_bar_top_level_entries;
 	string menu_bar_top_level_entries[4];
+
+	ofRectangle *
 };
 
