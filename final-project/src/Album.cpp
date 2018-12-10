18c6a72e917904e745aba1af6b4c4a4273603ab9
Jacob Babich <jacobbabichpublic+git@gmail.com>
2018-12-09 19:23:35 -0500

    Albums can store songs properly and can also be kept track of in the global unordered_map of album name (string) to album object (Album)

--- final-project/src/Album.cpp
+++ final-project/src/Album.cpp
@@ -9,9 +9,12 @@ Album::Album()
 
 Album::~Album()
 {
+	cout << "a new album object was made without args" << endl;
 }
 
 
 Album::Album(string name) {
 	this->name = name;
+
+	cout << "a new album object was made with args" << endl;
 }
